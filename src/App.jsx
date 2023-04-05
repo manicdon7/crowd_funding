c1580d611de4f2f7acd7742ee809cc8cd8629bd1
manicdon7 <manicdon7@gmail.com>
2023-04-05 17:14:13 +0530

    css commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
+    <div className="black">
+      <h1 className="bg-black text-white">Hello world!</h1>
       <h2>let's code</h2>
     </div>
   );
